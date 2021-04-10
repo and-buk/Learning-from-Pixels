760f34eaf92abb311d280df4cfb4543b0017361c
Andrei Bukalov <67602840+and-buk@users.noreply.github.com>
2021-04-10 16:17:27 -0300

    fix: A bug fix

--- double_dqn_agent.py
+++ double_dqn_agent.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from collections import namedtuple, deque
 
-from model_vb import VQNetwork
+from model import VQNetwork
 
 import torch
 import torch.nn.functional as F
